2d2cf6d4c74ffd8ea148d93a13d3d9274200a738
arzumanov2004 <sd8cxz70x@code.edu.az>
2023-10-22 18:15:20 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,163 @@
+// 1-ci sual.
+
+
+
+
+
+
+
+
+
+
+
+// 2-ci sual.
+
+
+// let a = 5
+// for (let i = 1; i <= 1000; i++) {
+//     if (i % a === 0) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 3-cu sual. 
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         console.log(arr[i]);
+    
+//     }
+    
+// }
+
+
+
+
+
+// 4-cu sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         console.log(arr[i]);
+//     }
+    
+// }
+
+
+
+
+// 5-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+
+// const min = arr [0];
+// const index = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < min) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 6-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// const max = arr[0];
+// const index = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         console.log(i);
+//     }
+    
+// }
+
+
+
+
+// 7-ci sual. 
+
+
+
+// const arr = [2, 4, 5, 9, 1];
+// let cem = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 === 1) { 
+//     cem += arr[i];
+//   }
+// }
+
+// console.log(cem);
+
+
+
+
+// 8-ci sual. 
+
+
+
+// let sentence = "Mən Code Academy Tələbəsiyəm";
+// let lowerCaseSentence = sentence.toLowerCase();
+// console.log(lowerCaseSentence);
+
+
+
+
+
+
+
+// 9-cu sual. 
+
+
+
+// let sentence =  "            Mən Code Academydə Programing tədrisi alıram            ";
+
+// console.log(sentence.trimStart());
+
+
+
+
+// 10-cu sual. 
+
+
+
+
+// let myEducation = "Mən Code Academy Tələbəsiyəm";
+// let words = myEducation.split(" ");
+
+// for (let i = 0; i < words.length; i++) {
+    
+//     console.log(words[i]);
+    
+// }
+
+
+
+
+// 11-ci sual. 
+
+
+
+// let sentence = "   Mən Code Academydə Programing tədrisi alıram   ";
+
+// let sentenceLength = sentence.replace(/[^A-Za-z]/g, "").length;
+
+// console.log(sentenceLength);
